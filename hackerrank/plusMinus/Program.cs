6fa849f792b76f6bf5c900e29fc375f7c5bb0ee8
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-17 15:40:42 -0400

    plusMinus complete

--- hackerrank/plusMinus/Program.cs
+++ hackerrank/plusMinus/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿//Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
 
 namespace plusMinus
 {
-    class Program
-    {
-        static void Main(string[] args)
+  class Solution {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        double positiveNums = 0;
+        double negativeNums = 0;
+        double zeroes = 0;
+        for (int i = 0; i < arr.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            if (arr[i] > 0)
+            {
+                positiveNums++;
+            }
+            else if (arr[i] < 0)
+            {
+                negativeNums++;
+            }
+            else
+            {
+                zeroes++;
+            }
         }
+
+        Console.WriteLine(positiveNums/arr.Length);
+        Console.WriteLine(negativeNums/arr.Length);
+        Console.WriteLine(zeroes/arr.Length);
+
     }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        plusMinus(arr);
+    }
+}
+
 }
